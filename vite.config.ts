398f323c7e4124311cfddbe082c1398c5bad0d5e
Just-Bax <isfandiyorbaxtiyorov132@gmail.com>
2023-10-25 18:59:54 +0500

    create

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
+  base: "/summary-boxes",
   plugins: [vue(), vueJsx()],
   css: {
     postcss: {
